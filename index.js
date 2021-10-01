1e2387e61d6feb83116771bc733d9ec591774559
Nicholas Herold <nherold86@gmail.com>
2021-09-30 20:03:19 -0400

    update

--- index.js
+++ index.js
@@ -1,13 +1,90 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions =  [
+    // Project Title
+    {
+        type: "input",
+        message: "Please provide name of project?",
+        name: "title",
+    },
+    // Enter Description
+    {
+        type: "input",
+        message: "Please provide a description of your project.",
+        name: "description",
+    },
+    // Installation
+    {
+        type: "input",
+        message: "Please provide commands to install any dependencies?",
+        name: "installation",
+    },
+    // Usage information
+    {
+        type: "input",
+        message: "How is this application meant to be used?",
+        name: "usage",
+    },
+    // contribution guidelines
+    {
+        type: "input",
+        message: "contribution guidelines for your project?",
+        name: "contribution",
+    },
+    // test instructions
+    {
+        type: "input",
+        message: "What command should be run for testing?",
+        name: "testing",
+    },
+    // License...change input to options(?)
+    // add license badge to top of readme and notice is added to the section of the readme entitled license that explains which license the app is covered under
+    {
+        type: "list",
+        message: "What license should your README have?",
+        choices: [
+            "MIT",
+            "Gnu v3",
+            "Unlicense"
+        ],
+        name: "license",
+    },
+    // Github username
+    {
+        type: "input",
+        message: "Enter GitHub name:",
+        name: "github",
+    },
+    // email
+    {
+        type: "input",
+        message: "Enter email: ",
+        name: "email",
+    },
+
+];
+;
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile(README, data, (err) =>
+    err ? console.log(err) : console.log("successfully README.md"));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log("Please answer the following questions to create readme");
+    inquirer.prompt(questions)
+    .then(data => {
+        const info = generateMarkdown(data);
+        
+        writeToFile("exREADME.md", info);
+})
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,72 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const badge = {
+    "MIT": "https://img.shields.io/badge/License-MIT-yellow.svg",
+    "GNU v3" : "https://img.shields.io/badge/License-GPLv3-blue.svg",
+
+  }
+  if (license in badge){
+    return `[![License: ${license}](${badge[license]})](${renderLicenseLink(license)})`;
+  } else 
+  // if unlicensed
+    return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return {
+    "MIT" : "https://opensource.org/licenses/MIT",
+    "GNU v3" : "https://www.gnu.org/licenses/gpl-3.0",
+  }[license]
 }
 
-module.exports = generateMarkdown;
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {
+//   if(license != "Unlicense"){
+//     return `This repository is under the [${license}](${renderLicenseLink(license)}).`
+// }
+
+// // TODO: Create a function to generate markdown for README
+// function generateMarkdown(data) {
+
+//   return `# ${data.title}
+
+//   ${renderLicenseBadge(data.license)}
+//     ## Description
+//    ${data.description}
+
+//   ## Table of Contents
+//   * [Installation](#installation)
+//   * [Usage](#usage)
+//   * [Contribution](#contribution)
+//   * [License](#license)
+//   * [Testing](#testing)
+//   * [Question](#questions)
+  
+//   ## Installation
+//   ${data.installation}
+  
+//   ## Usage
+//   ${data.usage}
+  
+//   ## License
+//   ${renderLicenseSection(data.license)}
+  
+//   ## Contribution
+//   ${data.contribution}
+  
+//   ## Testing
+//   ${data.testing}
+//   ## Questions
+  
+//   Please contact me at [${data.email}](mailto:${data.email}).  [${data.github}](https://github.com/${data.github}).
+  
+
+// `;
+// }
+
+// module.exports = generateMarkdown:
